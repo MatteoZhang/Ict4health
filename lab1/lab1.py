2787ff40e818a884f5a0d80d9c003559e0b37a2f
matteo <s223012@studenti.polito.it>
2018-11-03 16:22:31 +0100

    final fix

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,11 +3,12 @@ import numpy as np
 from sub.min import *
 
 if __name__ == "__main__":
+    np.random.seed(1)  # random seed grant us an comparable results obtained in each algorithm
     x = pd.read_csv("parkinsons_updrs.csv")
     x.info()  # the panda framework display an easier to read matrix
     realdata = x.values  # convert the values in the file into a matrix specifically an Ndarray
-    #np.random.shuffle(realdata)  # the real data is shuffled using numpy
-    np.random.seed(3)  # random seed grant us an comparable results obtained in each algorithm
+    np.random.shuffle(realdata)  # the real data is shuffled using numpy
+
 
     data = realdata[:, 4:22]  # specifications of the lab : neglect the first 4 columns
     Np, Nf = np.shape(data)  # Np(# of rows) is the number of patients and Nf(# of columns) is the number of features
@@ -37,15 +38,6 @@ if __name__ == "__main__":
     y_test = data_test_norm[:, F0]
     X_test = np.delete(data_test_norm, F0, 1)
 
-    '''y_train = data_train[:, F0]
-    X_train = np.delete(data_train, F0, 1)
-    X_train = np.insert(X_train, np.shape(X_train[0]), values=1, axis=1)
-    y_val = data_val[:, F0]
-    X_val = np.delete(data_val, F0, 1)
-    X_val = np.insert(X_val, np.shape(X_val[0]), values=1, axis=1)
-    y_test = data_test[:, F0]
-    X_test = np.delete(data_test, F0, 1)
-    X_test = np.insert(X_test, np.shape(X_test[0]), values=1, axis=1)'''
 
     # with the slicing operation we have to keep in mind to check the resulting shape
     # print(np.shape(y_train)) to check
@@ -58,45 +50,50 @@ if __name__ == "__main__":
     logy = 1
     Nit = 300
     gamma = 1e-5
+    mse_train = np.zeros((6,1),dtype=float)
+    mse_val = np.zeros((6,1),dtype=float)
+    mse_test = np.zeros((6,1),dtype=float)
 
     # these objects will be explained in the min file
-    m = SolveLLS(y_train, X_train, y_val, X_val)
-    m.run()
+    m = SolveLLS(y_train, X_train, y_val, X_val, y_test, X_test)
+    mse_train[0], mse_val[0],mse_test[0]=m.run()
     m.print_result('LLS')
-    m.print_hat('yhat_train vs y_train for LLs','yhat_train','y_train',y_train,X_train)
-    m.print_hat('yhat_test vs y_test for LLs', 'yhat_test', 'y_test', y_test, X_test)
+    m.print_hat('yhat_train vs y_train for LLs','yhat_train','y_train', y_train,X_train, mean[F0],std[F0])
+    m.print_hat('yhat_test vs y_test for LLs', 'yhat_test', 'y_test', y_test, X_test, mean[F0],std[F0])
 
-    g = SolveGrad(y_train, X_train, y_val, X_val)
-    g.run(1e-5, Nit)
+    g = SolveGrad(y_train, X_train, y_val, X_val, y_test, X_test)
+    mse_train[1], mse_val[1], mse_test[1]=g.run(1e-5, Nit)
     g.print_result('Gradient algorithm')
     g.plot_err('Gradient algorithm : square error', logy, logx)
-    g.print_hat('yhat_train vs y_train for Gradient', 'yhat_train', 'y_train', y_train, X_train)
-    g.print_hat('yhat_test vs y_test for Gradient', 'yhat_test', 'y_test', y_test, X_test)
+    g.print_hat('yhat_train vs y_train for Gradient', 'yhat_train', 'y_train', y_train, X_train, mean[F0],std[F0])
+    g.print_hat('yhat_test vs y_test for Gradient', 'yhat_test', 'y_test', y_test, X_test, mean[F0],std[F0])
 
-    sd = SolveSteepDesc(y_train, X_train, y_val, X_val)
-    sd.run(Nit)
+    sd = SolveSteepDesc(y_train, X_train, y_val, X_val, y_test, X_test)
+    mse_train[2], mse_val[2], mse_test[2] =sd.run(Nit)
     sd.print_result('Steepest Descent algorithm')
     sd.plot_err('Steepest Descent : square error', logy, logx)
-    sd.print_hat('yhat_train vs y_train for Steepest Descent', 'yhat_train', 'y_train', y_train, X_train)
-    sd.print_hat('yhat_test vs y_test for Steepest Descent', 'yhat_test', 'y_test', y_test, X_test)
+    sd.print_hat('yhat_train vs y_train for Steepest Descent', 'yhat_train','y_train',y_train, X_train,mean[F0],std[F0])
+    sd.print_hat('yhat_test vs y_test for Steepest Descent', 'yhat_test', 'y_test', y_test, X_test, mean[F0],std[F0])
 
-    st = SolveStoch(y_train, X_train, y_val, X_val)
-    st.run(Nit, Nf, gamma)
+    st = SolveStoch(y_train, X_train, y_val, X_val, y_test, X_test)
+    mse_train[3], mse_val[3], mse_test[3] =st.run(Nit, Nf, gamma)
     st.print_result('Stochastic gradient algorithm')
     st.plot_err('Stochastic gradient : square error', logy, logx)
-    st.print_hat('yhat_train vs y_train for Stochastic', 'yhat_train', 'y_train', y_train, X_train)
-    st.print_hat('yhat_test vs y_test for Stochastic', 'yhat_test', 'y_test', y_test, X_test)
+    st.print_hat('yhat_train vs y_train for Stochastic', 'yhat_train', 'y_train', y_train, X_train, mean[F0],std[F0])
+    st.print_hat('yhat_test vs y_test for Stochastic', 'yhat_test', 'y_test', y_test, X_test, mean[F0],std[F0])
 
-    conj = SolveConj(y_train, X_train, y_val, X_val)
-    conj.run()
+    conj = SolveConj(y_train, X_train, y_val, X_val, y_test, X_test)
+    mse_train[4], mse_val[4], mse_test[4] =conj.run()
     conj.print_result('Conjugate')
     conj.plot_err('Conjugate : square error', logy, logx)
-    conj.print_hat('yhat_train vs y_train for Conjugate', 'yhat_train', 'y_train', y_train, X_train)
-    conj.print_hat('yhat_test vs y_test for Conjugate', 'yhat_test', 'y_test', y_test, X_test)
+    conj.print_hat('yhat_train vs y_train for Conjugate', 'yhat_train', 'y_train', y_train, X_train, mean[F0],std[F0])
+    conj.print_hat('yhat_test vs y_test for Conjugate', 'yhat_test', 'y_test', y_test, X_test, mean[F0],std[F0])
 
-    ridge = SolveRidge(y_train, X_train, y_val, X_val)
-    ridge.run()
+    ridge = SolveRidge(y_train, X_train, y_val, X_val, y_test, X_test)
+    mse_train[5], mse_val[5], mse_test[5] =ridge.run()
     ridge.print_result('Ridge')
+    ridge.print_hat('yhat_train vs y_train for Ridge', 'yhat_train', 'y_train', y_train, X_train, mean[F0],std[F0])
+    ridge.print_hat('yhat_test vs y_test for Ridge', 'yhat_test', 'y_test', y_test, X_test, mean[F0],std[F0])
 
     m.plot_w('w:LLS')
     g.plot_w('w:Grad')
@@ -104,6 +101,9 @@ if __name__ == "__main__":
     st.plot_w('w:Stoch')
     conj.plot_w('w:Conjugate')
     ridge.plot_w('w:Ridge')
+
+    final_comparison = SolveMinProbl(y_train, X_train, y_val, X_val, y_test, X_test)
+    final_comparison.print_mse(mse_train, mse_val, mse_test)
 
 
 
--- lab1/sub/min.py
+++ lab1/sub/min.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 
 class SolveMinProbl:
     """
-    The class SolveMinProbl is created to inizialize all the variable and it will
-    provide methods to print the final results.
+    This class is created to inizialize all the variable and it will provide methods to print the final results.
 
     Methods
     -------
@@ -18,19 +17,22 @@ class SolveMinProbl:
     print_hat(self, title, xlabel, ylabel, y, A)
         Gives the plot of the actual regressand vs the regressand obtained from our algorithm
     """
-    def __init__(self, y, A, y_val, A_val):  # initialization of the variable
+    def __init__(self, y, A, y_val, A_val, y_test, A_test):  # initialization of the variable
         """
         Parameters
         ----------
-        :param y: regressand of the training set
-        :param A: regressor of the training set
-        :param y_val: regressand of the validation set
-        :param A_val: reggressor of the validation set
+        y: regressand of the training set
+        A: regressor of the training set
+        y_val: regressand of the validation set
+        A_val: reggressor of the validation set
+
         """
         self.matr = A  # allocations
         self.vect = y
         self.matr_val = A_val
         self.vect_val = y_val
+        self.matr_test = A_test
+        self.vect_test = y_test
         self.Np = A.shape[0]  # shape[0] is rows and Np represent Number of Patients
         self.Nf = A.shape[1]  # columns Nf represent Number of Features
         self.sol = np.zeros((self.Nf, 1), dtype=float)  # column vector w solution
@@ -42,9 +44,8 @@ class SolveMinProbl:
 
         Parameters
         ----------
-        :param title: str
+        title: str
             title of the solution vector w
-        :return:
         """
         w = self.sol  # already initialized self.sol in the previous method
         n = np.arange(self.Nf)  # number fo feature
@@ -65,9 +66,8 @@ class SolveMinProbl:
 
         Parameters
         ----------
-        :param title: str
+        title: str
             title of the results displayed in the console
-        :return:
         """
         print(title, ' :')
         print('the optimum weight vector is: ')
@@ -77,12 +77,11 @@ class SolveMinProbl:
         Parameters
         ----------
 
-        :param title: str
-        :param logy: int
+        title: str
+        logy: int
             0 if linear scale on y axis , the vertical one, 1 if log scale in y axis
-        :param logx: int
+        logx: int
             0 if linear on x axis , 1 if log scale on x axis
-        :return:
         """
         err = self.err
         plt.figure()
@@ -109,21 +108,29 @@ class SolveMinProbl:
         plt.margins(0.01, 0.1)
         plt.grid()
         plt.show()
-    def print_hat(self, title, xlabel, ylabel, y, A):
+    def print_hat(self, title, xlabel, ylabel, y, A, mean , std):
         """
         Paramenters
         -----------
-        :param title: str
-        :param xlabel: str
-        :param ylabel: str
-        :param y: float
-        :param A: float
-        :return:
+        title: str
+        xlabel: str
+            label for the x axis
+        ylabel: str
+            lab for the y axis
+        y: array
+            the values taken from the dataset
+        A: matrix
+            the values taken from the dataset
+        mean: float
+            scalar of the feature we want to regress on
+        std: float
+            scalar of the feature we want to regress on
         """
         plt.figure()
         w = self.sol
-        yhat = np.dot(A, w)
-        plt.plot(np.linspace(-3, 3), np.linspace(-3, 3), 'orange')
+        yhat = (np.dot(A, w) * std)+mean
+        y = (y * std)+mean
+        plt.plot(np.linspace(0, 60), np.linspace(0, 60), 'orange')
         plt.scatter(yhat, y, s=5)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
@@ -135,47 +142,71 @@ class SolveMinProbl:
         plt.title('histogram related to: '+title)
         plt.grid()
         plt.show()
+    def print_mse(self, mse_train,mse_val,mse_test):
+        """
+        Parameters
+        ----------
+        mse_train : array
+        mse_val : array
+        mse_test : array
+        """
+        plt.figure()
+        plt.title("Comparison of the means quare errors of the last interaction\n of each Algorithm")
+        plt.plot(np.arange(len(mse_train)), mse_train, label='train')
+        plt.plot(np.arange(len(mse_val)), mse_val, label='validation')
+        plt.plot(np.arange(len(mse_test)), mse_test, label='test')
+        plt.ylabel('error of the last interaction')
+        plt.xticks(ticks=range(6),
+                   labels=['LLS','Gradient','Steepest Descent','Stochastic','Conjugate','Ridge'], rotation='vertical')
+        plt.grid()
+        plt.legend()
+        plt.show()
 class SolveLLS(SolveMinProbl):  # this class belongs to SolveMinProbl
     """ Linear Least Square """
     def run(self):  # the input
         A = self.matr
         y = self.vect
+        A_val = self.matr_val
+        y_val = self.vect_val
+        A_test = self.matr_test
+        y_test = self.vect_test
+        self.err = np.zeros((1, 4), dtype=float)
         w = np.dot(np.dot(np.linalg.inv(np.dot(A.T, A)), A.T), y)
         self.sol = w
+        self.err[0, 1] = np.linalg.norm(np.dot(A, w)-y)**2/A.shape[0]
+        self.err[0, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 / A_val.shape[0]
+        self.err[0, 3] = np.linalg.norm(np.dot(A_test, w) - y_test) ** 2 / A_test.shape[0]
+        return self.err[0, 1], self.err[0, 2], self.err[0, 3]
 class SolveGrad(SolveMinProbl):
     """ Gradient Algorithm """
     def run(self, gamma, Nit):
-        """
-        Parameters
-        ----------
-        :param gamma: float
-            it should be small otherwise the Algorithm will be not very precise
-        :param Nit: int
-            choose it according to the wanted error value
-        :return:
-        """
-        self.err = np.zeros((Nit, 3), dtype=float)
+        self.err = np.zeros((Nit, 4), dtype=float)
         A = self.matr
         y = self.vect
         A_val = self.matr_val
         y_val = self.vect_val
+        A_test = self.matr_test
+        y_test = self.vect_test
         w = np.random.rand(self.Nf, 1)  # uniform pdf random var range 0,1
         for it in range(Nit):
             grad = 2*np.dot(A.T, (np.dot(A, w)-y))
             w = w - gamma * grad
             self.err[it, 0] = it
-            self.err[it, 1] = np.linalg.norm(np.dot(A, w)-y)**2/self.Np
-            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w)-y_val)**2/(self.Np/2)
+            self.err[it, 1] = np.linalg.norm(np.dot(A, w)-y)**2/A.shape[0]
+            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w)-y_val)**2/A_val.shape[0]
+            self.err[it, 3] = np.linalg.norm(np.dot(A_test, w) - y_test) ** 2 / A_test.shape[0]
         self.sol = w
-        '''mse_g = self.err[it, 1]
-        mse_gval = self.err[it, 2]'''
+        return self.err[-1, 1], self.err[-1, 2], self.err[-1, 3]
 class SolveSteepDesc(SolveMinProbl):
+    """ Solve Steepest Descend Algorithm """
     def run(self, Nit):
-        self.err = np.zeros((Nit, 3), dtype=float)
+        self.err = np.zeros((Nit, 4), dtype=float)
         A = self.matr
         y = self.vect
         A_val = self.matr_val
         y_val = self.vect_val
+        A_test = self.matr_test
+        y_test = self.vect_test
         w = np.random.rand(self.Nf, 1)  # uniform pdf random var range 0,1
         it = 0
         for it in range(Nit):
@@ -184,16 +215,21 @@ class SolveSteepDesc(SolveMinProbl):
             gamma2 = (np.linalg.norm(grad)**2)/np.dot(np.dot(grad.T, H), grad)
             w = w - gamma2 * grad
             self.err[it, 0] = it
-            self.err[it, 1] = np.linalg.norm(np.dot(A, w) - y) ** 2 / self.Np
-            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 / (self.Np / 2)
+            self.err[it, 1] = np.linalg.norm(np.dot(A, w) - y) ** 2 / A.shape[0]
+            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 /A_val.shape[0]
+            self.err[it, 3] = np.linalg.norm(np.dot(A_test, w) - y_test) ** 2 / A_test.shape[0]
         self.sol = w
+        return self.err[-1, 1], self.err[-1, 2], self.err[-1, 3]
 class SolveStoch(SolveMinProbl):
+    """ Stochastic Algorithm """
     def run(self, Nit, Nf, gamma):
-        self.err = np.zeros((Nit, 3), dtype=float)
+        self.err = np.zeros((Nit, 4), dtype=float)
         A = self.matr
         y = self.vect
         A_val = self.matr_val
         y_val = self.vect_val
+        A_test = self.matr_test
+        y_test = self.vect_test
         w = np.random.rand(self.Nf, 1)  # uniform pdf random var range 0,1
         it = 0
         row = np.zeros((1, self.Nf), dtype=float)
@@ -204,18 +240,23 @@ class SolveStoch(SolveMinProbl):
                 grad = 2 * row.T * (np.dot(row, w) - y[i])
                 w = w - gamma * grad
             self.err[it, 0] = it
-            self.err[it, 1] = np.linalg.norm(np.dot(A, w) - y) ** 2 / self.Np
-            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 / (self.Np /2)
+            self.err[it, 1] = np.linalg.norm(np.dot(A, w) - y) ** 2 / A.shape[0]
+            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 / A_val.shape[0]
+            self.err[it, 3] = np.linalg.norm(np.dot(A_test, w) - y_test) ** 2 / A_test.shape[0]
         self.sol = w
+        return self.err[-1, 1], self.err[-1, 2], self.err[-1, 3]
 class SolveMini(SolveMinProbl):
+    """" MiniBatch Algorithm """
     def run(self, Nit, N, gamma): #N is number of minibatches
         if (N > self.Np) | (self.Np % N != 0):
             print("function not valid")
-        self.err = np.zeros((Nit, 3), dtype=float)
+        self.err = np.zeros((Nit, 4), dtype=float)
         A = self.matr
         y = self.vect
         A_val = self.matr_val
         y_val = self.vect_val
+        A_test = self.matr_test
+        y_test = self.vect_test
         w = np.random.rand(self.Nf, 1)  # uniform pdf random var range 0,1
         it = 0
         m = int(self.Np/N)
@@ -225,16 +266,21 @@ class SolveMini(SolveMinProbl):
                     grad = 2 * np.dot(A[j:(j*m+m), :].T, (np.dot(A[j:(j*m+m), :], w) - y[j:(j*m+m)]))
                     w = w - gamma * grad
             self.err[it, 0] = it
-            self.err[it, 1] = np.linalg.norm(np.dot(A, w) - y) ** 2 / self.Np
-            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 / (self.Np /2)
+            self.err[it, 1] = np.linalg.norm(np.dot(A, w) - y) ** 2 / A.shape
+            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 / A_val.shape[0]
+            self.err[it, 3] = np.linalg.norm(np.dot(A_test, w) - y_test) ** 2 / A_test.shape[0]
         self.sol = w
+        return self.err[-1, 1], self.err[-1, 2], self.err[-1, 3]
 class SolveConj(SolveMinProbl):
+    """" Conjugate Algorithm """
     def run(self):
-        self.err = np.zeros((self.Nf, 3), dtype=float)
+        self.err = np.zeros((self.Nf, 4), dtype=float)
         A = self.matr
         y = self.vect
         A_val = self.matr_val
         y_val = self.vect_val
+        A_test = self.matr_test
+        y_test = self.vect_test
         w = np.zeros((self.Nf, 1), dtype=float)
         it = 0
         Q = 2 * np.dot(A.T, A)
@@ -250,27 +296,36 @@ class SolveConj(SolveMinProbl):
             beta = np.dot(np.dot(g.T, Q), d)/np.dot(np.dot(d.T, Q), d)
             d = -1 * g + beta*d
             self.err[it, 0] = it
-            self.err[it, 1] = np.linalg.norm(np.dot(A, w) - y) ** 2 / self.Np
-            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 / (self.Np /2)
+            self.err[it, 1] = np.linalg.norm(np.dot(A, w) - y) ** 2 / A.shape[0]
+            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 / A_val.shape[0]
+            self.err[it, 3] = np.linalg.norm(np.dot(A_test, w) - y_test) ** 2 / A_test.shape[0]
         self.sol = w
+        return self.err[-1, 1], self.err[-1, 2], self.err[-1, 3]
 class SolveRidge(SolveMinProbl):
+    """" Ridge Algorithm """
     def run(self):
         A = self.matr
         y = self.vect
         A_val = self.matr_val
         y_val = self.vect_val
+        A_test = self.matr_test
+        y_test = self.vect_test
         I = np.eye(self.Nf)
-        lamb = 150
-        self.err = np.zeros((lamb, 3), dtype=float)
-        for it in range(lamb):
-            w = np.dot(np.dot(np.linalg.inv(np.dot(A.T, A)+float(it*5/lamb)*I), A.T), y)
-            self.err[it, 0] = float(it/lamb)
-            self.err[it, 1] = np.linalg.norm(np.dot(A, w) - y) ** 2 / self.Np
-            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 / (self.Np /2)
-        best_lamb = 0.01
+        stop_lamb = 100
+        self.err = np.zeros((stop_lamb, 4), dtype=float)
+        for it in range(stop_lamb):
+            w = np.dot(np.dot(np.linalg.inv(np.dot(A.T, A)+float(it)*I), A.T), y)
+            self.err[it, 0] = it
+            self.err[it, 1] = np.linalg.norm(np.dot(A, w) - y) ** 2 / A.shape[0]
+            self.err[it, 2] = np.linalg.norm(np.dot(A_val, w) - y_val) ** 2 / A_val.shape[0]
+            self.err[it, 3] = np.linalg.norm(np.dot(A_test, w) - y_test) ** 2 / A_test.shape[0]
+        best_lamb = np.argmin(self.err[:, 2])
         w = np.dot(np.dot(np.linalg.inv(np.dot(A.T, A) + best_lamb * I), A.T), y)
         self.sol = w
         err = self.err
+        print("best lambda is :", best_lamb)
+
+        #plotting the figure of the ridge respect to the lambda which is from 0-1
         plt.figure()
         plt.semilogy(err[:, 0], err[:, 1], label='train')
         plt.semilogy(err[:, 0], err[:, 2], label='val')
@@ -281,3 +336,4 @@ class SolveRidge(SolveMinProbl):
         plt.margins(0.01, 0.1)
         plt.grid()
         plt.show()
+        return self.err[-1, 1], self.err[-1, 2], self.err[-1, 3]
