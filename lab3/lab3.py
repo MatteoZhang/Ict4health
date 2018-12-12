36ba421dad1b2b47be58932b996e7080c0a949cc
matteo <s223012@studenti.polito.it>
2018-12-12 18:09:57 +0100

    A

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -2,31 +2,6 @@ import pandas as pd
 import numpy as np
 from minimization import *
 
-
-class FillNan(object):
-    def __init__(self, matrix_to_regress, matrix_train, mean, std, N):
-        self.matrix = matrix_to_regress
-        self.matrix_train = matrix_train
-        self.mean = mean
-        self.std = std
-        self.N = N
-        self.Np = self.matrix.shape[0]
-        self.Nf = self.matrix.shape[1]
-
-    def fill_all_nan(self):
-        j = 0
-        for array in self.matrix
-            i = 0
-            j += 1
-            F0 = []
-            for i in range(self.matrix.shape[1]):
-                if np.isnan(array[i]):
-                    F0 = F0.append[i]
-            y_train = self.[:, F0]
-            X_train = np.delete(X_norm, F0, 1)
-            y_train = y_train.reshape(y_train.shape[0], 1)
-
-
 # link = https://archive.ics.uci.edu/ml/datasets/chronic_kidney_disease
 if __name__ == "__main__":
     column = np.arange(25)
@@ -47,10 +22,6 @@ if __name__ == "__main__":
                                'notckd': 0, ' notckd': 0, '\tnotckd': 0, 'notckd\t': 0})
 
     X_0 = data_polished.dropna(thresh=25).values.astype(float)
-    X_1 = data_polished.dropna(thresh=24).values.astype(float)
-    X_2 = data_polished.dropna(thresh=23).values.astype(float)
-    X_3 = data_polished.dropna(thresh=22).values.astype(float)
-    X_4 = data_polished.dropna(thresh=21).values.astype(float)
     X_5 = data_polished.dropna(thresh=20).values.astype(float)
     Np, Nf = np.shape(X_5)
 
@@ -61,28 +32,33 @@ if __name__ == "__main__":
     X_norm = (X_0 - mean)/std
 
     j = 0
-    for array in X_1:
+    for array in X_5:
         i = 0
         j += 1
+        F0 = []
         # print("array: ", array.shape[0])
         # print("X_1 shape: ", X_1.shape[1])
-        for i in range(X_1.shape[1]):
+        for i in range(X_5.shape[1]):
             # print(array[i])
             if np.isnan(array[i]):
-                F0 = i
-        y_train = X_norm[:, F0]
-        X_train = np.delete(X_norm, F0, 1)
-        y_train = y_train.reshape(y_train.shape[0], 1)
-        ridge = SolveRidge(y_train, X_train)
-        w = ridge.run(lamb=10)
-        # ridge.plot_w('optimum weight vector for Ridge Regression')
-        tmp_mean = np.copy(np.delete(mean, F0))
-        tmp_std = np.copy(np.delete(std, F0))
-        array_train = (array[~np.isnan(array)] - tmp_mean) / tmp_std
-        y_hat = np.dot(array_train, w) * std[0, F0] + mean[0, F0]
-        array[i] = y_hat
-        X_1[j] = array
+                F0.append(i)
+                # F0 are index of the y columns
+        if len(F0) != 0:
+            y_train = X_norm[:, F0]
+            X_train = np.delete(X_norm, F0, 1)
+            ridge = SolveRidge(y_train, X_train)
+            w = ridge.run(lamb=10)
+            tmp_mean = np.copy(np.delete(mean, F0))
+            tmp_std = np.copy(np.delete(std, F0))
+            tmp_mean = tmp_mean.reshape(tmp_mean.shape[0], 1)
+            tmp_std = tmp_std.reshape(tmp_std.shape[0], 1)
+            array_train = (array[~np.isnan(array)] - tmp_mean) / tmp_std
+            y_hat = np.dot(array_train, w) * std[0, F0] + mean[0, F0]
+            for index in range(len(F0)):
+                array[F0[index]] = y_hat[index]
+            X_5[j] = array
+    # rounding the values
 
     # extend our method
-    fill_2 = FillNan(X_2, X_norm, mean, std, 2)
-    X_2 = fill_2.fill_all_nan()
+    print("----END----")
+
